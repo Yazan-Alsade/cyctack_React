a5f022571a22b5a344809955c5e6d97ee08fd135
Yazan-Alsade <yazanalsade327@gmail.com>
2023-12-30 21:22:50 +0200

    add checkbox to filter the expired certificate

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,15 +1,20 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Table, Checkbox } from 'antd';
+import './datatable.css';
 
 const DataTable = () => {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [showExpired, setShowExpired] = useState(false);
+  const [filteredValue, setFilteredValue] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://crt.sh/?q=cystack.ps&output=json');
-        console.log(response.data)
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -19,7 +24,6 @@ const DataTable = () => {
     fetchData();
   }, []);
 
-  // Function to format the timestamp
   const formatTimestamp = (timestamp) => {
     const date = new Date(timestamp);
     const year = date.getFullYear();
@@ -28,33 +32,90 @@ const DataTable = () => {
     return `${year}/${month}/${day}`;
   };
 
-  const columns = ['crt.sh ID', 'Logged At', 'Not Before', 'Not After','Common Name','Matching Identities','Issuer Name'];
+  const columns = [
+    {
+      title: 'Certificate',
+      dataIndex: 'id',
+      key: 'rowNumber',
+      render: (text, record, index) => {
+        return (currentPage - 1) * pageSize + index + 1;
+      },
+    },
+    {
+      title: 'crt.sh ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Logged At',
+      dataIndex: 'entry_timestamp',
+      key: 'entry_timestamp',
+      render: (text) => formatTimestamp(text),
+    },
+    {
+      title: 'Not Before',
+      dataIndex: 'not_before',
+      key: 'not_before',
+      render: (text) => formatTimestamp(text),
+    },
+    {
+      title: 'Not After',
+      dataIndex: 'not_after',
+      key: 'not_after',
+      render: (text) => formatTimestamp(text),
+      filteredValue: filteredValue,
+      filters: [
+        {
+          text: 'Expired',
+          value: true,
+        },
+        {
+          text: 'Not Expired',
+          value: false,
+        },
+      ],
+      onFilter: (value, record) => {
+        const isExpired = new Date(record.not_after) < new Date();
+        return value ? isExpired : !isExpired;
+      },
+    },
+    {
+      title: 'Common Name',
+      dataIndex: 'common_name',
+      key: 'common_name',
+    },
+    {
+      title: 'Matching Identities',
+      dataIndex: 'name_value',
+      key: 'name_value',
+    },
+    {
+      title: 'Issuer Name',
+      dataIndex: 'issuer_name',
+      key: 'issuer_name',
+    },
+  ];
 
   return (
     <div>
-      <h2>Data Table</h2>
-      <table border="1">
-        <thead>
-          <tr>
-            {columns.map(column => (
-              <th key={column}>{column}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map(row => (
-            <tr key={row.id}>
-              <td>{row.id}</td>
-              <td>{formatTimestamp(row.entry_timestamp)}</td>
-              <td>{formatTimestamp(row.not_before)}</td>
-              <td>{formatTimestamp(row.not_after)}</td>
-              <td>{row.common_name}</td>
-              <td>{row.name_value}</td>
-              <td>{row.issuer_name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+     
+      <Table
+        dataSource={data}
+        columns={columns}
+        pagination={{
+          current: currentPage,
+          total: data.length,
+          pageSize: pageSize,
+          onChange: (currentPage, pageSize) => {
+            setCurrentPage(currentPage);
+            setPageSize(pageSize);
+          },
+        }}
+        rowKey="id"
+        onChange={(pagination, filters) => {
+          setFilteredValue(filters.not_after);
+        }}
+      />
     </div>
   );
 };
